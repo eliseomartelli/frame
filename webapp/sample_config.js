78fe33d4ed4d124f691099f82903beabd233bfc5
Eliseo Martelli <me@eliseomartelli.it>
2019-02-24 16:02:38 +0100

    add webapp

---
+++ webapp/sample_config.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "apikey",
+    authDomain: "domain",
+    databaseURL: "url",
+    projectId: "id",
+    storageBucket: "id",
+    messagingSenderId: "1234"
+};
+firebase.initializeApp(config);
+
+var mac = "";
