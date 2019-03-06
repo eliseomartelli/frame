9edc1ab3b981786c0361a8454a1194dafa90f3d2
Eliseo Martelli <me@eliseomartelli.it>
2019-03-06 14:03:05 +0100

    auth separation

--- controller/src/App.js
+++ controller/src/App.js
@@ -6,14 +6,42 @@ import '../node_modules/material-design-lite/material.min.css';
 import Toolbar from './modules/toolbar.js'
 import Auth from './modules/auth.js'
 
+import firebase from 'firebase';
+import config from './config.js';
+
+firebase.initializeApp(config);
+
 class App extends Component {
+
+  state = {
+    isSignedIn: false // Local signed-in state.
+  };
+
+  componentDidMount() {
+    this.unregisterAuthObserver = firebase.auth().onAuthStateChanged(
+      (user) => this.setState({isSignedIn: !!user})
+    );
+  }
+
+  componentWillUnmount() {
+    this.unregisterAuthObserver();
+  }
+
   render() {
+    var toshow = ((this.state.isSignedIn) ?
+                  <Auth firebase={firebase}/> : '');
     return (
       <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
         <Toolbar />
         <main className="mdl-layout__content">
           <div className="page-content">
-            <Auth />
+            <div className="mdl-grid">
+              <div className="mdl-layout-spacer"></div>
+              <div className="mdl-cell mdl-cell--4-col">
+                {toshow}
+              </div>
+              <div className="mdl-layout-spacer"></div>
+            </div>
           </div>
         </main>
       </div>
